6633c85c7118484a144f558ca3c09cb4ea185cec
phamr39 <pha.mr3998@gmail.com>
2020-09-09 22:12:46 +0700

    update code

--- RFID/RFID_RasPi.py
+++ RFID/RFID_RasPi.py
@@ -15,6 +15,9 @@ import signal
 # from flask import Flask, render_template
 from smbus2 import SMBus
 from mlx90614 import MLX90614
+import board
+import busio
+import adafruit_mlx90640
 # from firebase import firebase
 # Variable define 
 hooman = ''
@@ -247,9 +250,61 @@ class FireBase_Com:
 # def hello():
 #     return render_template('attendance.html')
 # --------------------------------------------- #
+def TestMLX():
+    PRINT_TEMPERATURES = False
+    PRINT_ASCIIART = True
+    
+    i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
+    
+    mlx = adafruit_mlx90640.MLX90640(i2c)
+    print("MLX addr detected on I2C")
+    print([hex(i) for i in mlx.serial_number])
+    
+    mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
+    
+    frame = [0] * 768
+    while True:
+    stamp = time.monotonic()
+    try:
+        mlx.getFrame(frame)
+    except ValueError:
+        # these happen, no biggie - retry
+        continue
+    print("Read 2 frames in %0.2f s" % (time.monotonic() - stamp))
+    for h in range(24):
+        for w in range(32):
+            t = frame[h * 32 + w]
+            if PRINT_TEMPERATURES:
+                print("%0.1f, " % t, end="")
+            if PRINT_ASCIIART:
+                c = "&"
+                # pylint: disable=multiple-statements
+                if t < 20:
+                    c = " "
+                elif t < 23:
+                    c = "."
+                elif t < 25:
+                    c = "-"
+                elif t < 27:
+                    c = "*"
+                elif t < 29:
+                    c = "+"
+                elif t < 31:
+                    c = "x"
+                elif t < 33:
+                    c = "%"
+                elif t < 35:
+                    c = "#"
+                elif t < 37:
+                    c = "X"
+                # pylint: enable=multiple-statements
+                print(c, end="")
+        print()
+    print()
 if __name__ == "__main__":
     print("Starting...")
-    FireBase_Com.Init()
+    # FireBase_Com.Init()
     # FireBase_Com.TestEvent()
-    RFID.RFIDTask()
+    # RFID.RFIDTask()
+    TestMLX()
 
